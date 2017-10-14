6bfb65f94133ead991aab360928ffca0d9e998da
Balazs Dravai <balazs.dravai@p92.hu>
2017-10-14 15:24:04 +0200

    third

--- app/Services.php
+++ app/Services.php
@@ -10,25 +10,4 @@ class Services
     {
         $this->controller = $controller;
     }
-
-    public function userRelationShip($userid, $friendid, $status = 0)
-    {
-        $relationship = $this->controller->getModel()->getDatabase()->selectFromWhere(
-            'user_friend',
-            "(user_id = $userid AND friend_id = $friendid AND status = $status) OR (user_id = $friendid AND friend_id = $userid AND status = $status)"
-            , '');
-
-        return count($relationship) > 0;
-    }
-
-    public function userKnownRelationShip($userid, $friendid)
-    {
-        return $this->userRelationShip($userid, $friendid, 1);
-    }
-
-
-    public function userPendingRelationShip($userid, $friendid)
-    {
-        return $this->userRelationShip($userid, $friendid, 0);
-    }
 }
